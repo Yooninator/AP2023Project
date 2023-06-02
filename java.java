24d2253a4faaf1f60be409c38d21f63050af3f1a
EC2 Default User <ec2-user@ip-172-31-3-84.ec2.internal>
2023-06-02 05:48:49 +0000

    first

---
+++ java.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class java {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] choices = {"rock", "paper", "scissors"};
+
+        System.out.println("Let's play Rock-Paper-Scissors!");
+
+        while (true) {
+            System.out.print("Enter your choice (rock, paper, or scissors): ");
+            String playerChoice = scanner.nextLine().toLowerCase();
+
+            if (!playerChoice.equals("rock") && !playerChoice.equals("paper") && !playerChoice.equals("scissors")) {
+                System.out.println("Invalid choice. Please try again.");
+                continue;
+            }
+
+            int randomIndex = random.nextInt(choices.length);
+            String computerChoice = choices[randomIndex];
+
+            System.out.println("Computer chooses: " + computerChoice);
+
+            if (playerChoice.equals(computerChoice)) {
+                System.out.println("It's a tie!");
+            } else if ((playerChoice.equals("rock") && computerChoice.equals("scissors")) ||
+                    (playerChoice.equals("paper") && computerChoice.equals("rock")) ||
+                    (playerChoice.equals("scissors") && computerChoice.equals("paper"))) {
+                System.out.println("You win!");
+            } else {
+                System.out.println("Computer wins!");
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            String playAgain = scanner.nextLine().toLowerCase();
+            if (!playAgain.equals("yes")) {
+                break;
+            }
+        }
+
+        System.out.println("Thanks for playing!");
+        scanner.close();
+    }
+}
